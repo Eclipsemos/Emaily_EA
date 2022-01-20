de2d9ac0bb54d054b340f6876cf3f7470fdc2864
Eclipsemos <mos7cy@163.com>
2022-01-20 12:26:34 -0600

    Built the basic client forms

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Client Page
         </p>
-        <p>
-          HI there
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/auth/google">Sign In With Google</a>
+        <button type="button">Sign In With Google</button>
       </header>
     </div>
   );
